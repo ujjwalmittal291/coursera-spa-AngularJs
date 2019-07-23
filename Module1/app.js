29bbf9a87bc148a5330d03240b0cd6d8ddd4bfd7
Ujjwal Mittal <ujjwalmittal@live.com>
2019-07-23 22:01:14 +0530

    Update app.js

--- Module1/app.js
+++ Module1/app.js
@@ -1,16 +1,12 @@
 (function() {
     'use strict';
-
     angular.module('LunchCheck', [])
         .controller('LunchCheckController', LunchCheckController);
-
     LunchCheckController.$inject = ['$scope'];
-
     function LunchCheckController($scope) {
         $scope.dishes = '';
         $scope.message = '';
         $scope.checked = false;
-
         $scope.checkLunch = function() {
             if ($scope.dishes.trim().length === 0) {
                 $scope.empty = true;
@@ -21,7 +17,6 @@
                 var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
                     return v.trim() !== '';
                 });
-
                 if (arrayDishesWithoutEmptys.length <= 3) {
                     $scope.message = 'Enjoy!';
                 } else {
